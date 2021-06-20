c4a34a37b6871192c8c2360f6a3521571e754223
AmiraslanSumgait <83180686+AmiraslanSumgait@users.noreply.github.com>
2021-06-21 00:08:33 +0400

    Update Client.cs

--- ConsoleApp1/ConsoleApp1/Client.cs
+++ ConsoleApp1/ConsoleApp1/Client.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Simple_Atm
 {
     class Client
     {
