b0253caea1b30d4af6f9b556104cc6dc382b1ea5
AmiraslanSumgait <83180686+AmiraslanSumgait@users.noreply.github.com>
2021-06-21 00:07:33 +0400

    Update Bank.cs

--- ConsoleApp1/ConsoleApp1/Bank.cs
+++ ConsoleApp1/ConsoleApp1/Bank.cs
@@ -1,20 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Simple_Atm
 {
     class Bank
     {
-        public Client[]clients { get; set; }
+        public Client[] Clients { get; set; }
         public void showCardBalance(Bankcard bankcard)
         {
 
             Console.WriteLine($"Bankcard Balance:{bankcard.Balance}");
         }
-        public   void AddClient(Client client)
+        public void AddClient(Client client)
         {
-            clients.Length = clients.Length + 1;
+            Clients = Clients.Concat(new Client[] { client }).ToArray();
+        }
+        public void DeleteCLient(string name)
+        {
+            int clientCount = Clients.Length - 1;
+            Client[] clients = new Client[clientCount];
+            for (int i = 0; i < Clients.Length; i++)
+            {
+                if (Clients[i].name!= name)
+                {
+                    clients[i] = Clients[i];
+                }
+            }
+            Clients = clients;
         }
     }
 }
